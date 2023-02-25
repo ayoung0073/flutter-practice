7d25c42adbc8f48f3aeb5d6f54be45f917c51841
ayoung0073 <ayong0310@naver.com>
2023-02-25 16:00:21 +0900

    feat: initState, dispose 메서드 연습

--- toonflix/lib/main.dart
+++ toonflix/lib/main.dart
@@ -13,13 +13,12 @@ class MyWidget extends StatefulWidget {
 }
 
 class _MyWidgetState extends State<MyWidget> {
-  void onClicked() {
-    // setState(): State 클래스에게 데이터가 변경되었다고 알리는 함수다. => build 메서드를 다시 실행하도록 한다.
-    // 이 메서드를 호출하지 않으면 build 메서드는 다시 실행되지 않는다.
+  bool showTitle = true;
 
-    // counter = counter + 1;
-    // setState(() {});
-    // 이렇게도 가능하지만, 가독성을 위해 안으로 넣는 것이 좋다.
+  void toggleTitle() {
+    setState(() {
+      showTitle = !showTitle;
+    });
   }
 
   @override
@@ -38,8 +37,12 @@ class _MyWidgetState extends State<MyWidget> {
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: const [
-              MyLargeTitle(),
+            children: [
+              showTitle ? const MyLargeTitle() : const Text('nothing'),
+              IconButton(
+                onPressed: toggleTitle,
+                icon: const Icon(Icons.remove_red_eye),
+              )
             ],
           ),
         ),
@@ -49,10 +52,27 @@ class _MyWidgetState extends State<MyWidget> {
 }
 
 // _MyWidgetState 에서 별도의 위젯(MyLargeTitle) 분리
-class MyLargeTitle extends StatelessWidget {
+class MyLargeTitle extends StatefulWidget {
   const MyLargeTitle({
     super.key,
   });
+
+  @override
+  State<MyLargeTitle> createState() => _MyLargeTitleState();
+}
+
+class _MyLargeTitleState extends State<MyLargeTitle> {
+  @override
+  void initState() {
+    super.initState();
+    print("initState");
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    print("dispose");
+  }
 
   @override
   Widget build(BuildContext context) {
