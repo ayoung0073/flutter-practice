460a78052fd828ff34fb0d25832cc75d4342343d
ayoung0073 <ayong0310@naver.com>
2023-02-25 15:26:44 +0900

    feat: build context를 이용한 부모 요소 데이터 접근

--- toonflix/lib/main.dart
+++ toonflix/lib/main.dart
@@ -13,14 +13,9 @@ class MyWidget extends StatefulWidget {
 }
 
 class _MyWidgetState extends State<MyWidget> {
-  int counter = 0;
-
   void onClicked() {
     // setState(): State 클래스에게 데이터가 변경되었다고 알리는 함수다. => build 메서드를 다시 실행하도록 한다.
     // 이 메서드를 호출하지 않으면 build 메서드는 다시 실행되지 않는다.
-    setState(() {
-      counter = counter + 1;
-    });
 
     // counter = counter + 1;
     // setState(() {});
@@ -31,33 +26,41 @@ class _MyWidgetState extends State<MyWidget> {
   Widget build(BuildContext context) {
     // 데이터가 변경될 때 위젯도 함께 스스로 업데이트된다.
     return MaterialApp(
+      theme: ThemeData(
+        textTheme: const TextTheme(
+          titleLarge: TextStyle(
+            color: Colors.red,
+          ),
+        ),
+      ),
       home: Scaffold(
         backgroundColor: const Color(0xFFF4EDDB),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text(
-                'Click Count',
-                style: TextStyle(
-                  fontSize: 20,
-                ),
-              ),
-              Text(
-                '$counter',
-                style: const TextStyle(
-                  fontSize: 20,
-                ),
-              ),
-              IconButton(
-                  iconSize: 40,
-                  onPressed: onClicked,
-                  icon: const Icon(
-                    Icons.add_box_rounded,
-                  ))
+            children: const [
+              MyLargeTitle(),
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+// _MyWidgetState 에서 별도의 위젯(MyLargeTitle) 분리
+class MyLargeTitle extends StatelessWidget {
+  const MyLargeTitle({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      'My Large Title',
+      style: TextStyle(
+        fontSize: 20,
+        color: Theme.of(context).textTheme.titleLarge?.color,
       ),
     );
   }
