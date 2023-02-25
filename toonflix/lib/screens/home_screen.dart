90e7ccd32b5e4e83a853af8d37b83f352f95bef1
ayoung0073 <ayong0310@naver.com>
2023-02-25 18:00:44 +0900

    feat: add timer

--- toonflix/lib/screens/home_screen.dart
+++ toonflix/lib/screens/home_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -8,6 +10,25 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int totalSeconds = 60 * 25; // 25분
+  late Timer timer;
+
+  void onStartPressed() {
+    timer = Timer.periodic(
+      // 1초마다 이 안에 있는 함수를 실행시킨다.
+      const Duration(
+        seconds: 1,
+      ),
+      onTick, // 괄호()를 넣으면 함수를 바로 실행하는 걸 의미하는 것이기 때문에 생략해야 한다.
+    );
+  }
+
+  void onTick(Timer timer) {
+    setState(() {
+      totalSeconds = totalSeconds - 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,7 +40,7 @@ class _HomeScreenState extends State<HomeScreen> {
             child: Container(
               alignment: Alignment.bottomCenter,
               child: Text(
-                "25:00",
+                "$totalSeconds",
                 style: TextStyle(
                   color: Theme.of(context).cardColor,
                   fontSize: 80,
@@ -32,7 +53,7 @@ class _HomeScreenState extends State<HomeScreen> {
             flex: 2,
             child: Center(
               child: IconButton(
-                onPressed: () {},
+                onPressed: onStartPressed,
                 iconSize: 100,
                 icon: Icon(
                   Icons.play_circle_outlined,
@@ -49,6 +70,7 @@ class _HomeScreenState extends State<HomeScreen> {
                   child: Container(
                     decoration: BoxDecoration(
                       color: Theme.of(context).cardColor,
+                      borderRadius: BorderRadius.circular(30),
                     ),
                     child: Column(
                       mainAxisAlignment: MainAxisAlignment.center,
